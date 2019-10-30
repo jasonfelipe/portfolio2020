2318644b55e84321fac1647e0e699d96fa507efb
Jason Felipe <jasonrfelipe@gmail.com>
2019-10-30 12:03:04 -0700

    adding functionality to techbar on main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Hero from '../components/Hero';
+import Techbar from '../components/Techbar';
 import Card from '../components/Card';
 
 const Main = () => {
-    const handleClick = event => {
-        console.log('Hey lol');
+    const [search, setSearch] = useState(null); 
+    const handleTechQuery = event => {
+        let { id } = event.target;
+        console.log('Hey lol', id);
     }
 
     return <>
+        <Techbar handleTechBarClick={handleTechQuery}/>
         <Card
             title='Example'
             text='something'
