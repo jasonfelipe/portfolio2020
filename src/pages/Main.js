0e7975847de01bc747273c3f9cbf796adb26868e
Jason Felipe <jasonrfelipe@gmail.com>
2019-10-31 11:10:46 -0700

    added project json functionality

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,43 +1,41 @@
 import React, {useState} from 'react';
 import Techbar from '../components/Techbar';
 import Card from '../components/Card';
+import { Projects } from '../assets/projects.json'
 import './main.css';
+console.log(Projects)
 
 const Main = () => {
     const [search, setSearch] = useState(null);
-    const [projects, setProjects] = useState([{title: "Hey", text: "Hello"}]);
-
+    const [projects, setProjects] = useState(Projects);
 
     const handleTechQueryButton = event => {
         let { id } = event.target;
         setSearch(id);
     }
 
-    const handleSearch = (searchArray = null, projectsJson) => {
-        let shownProjects = []
-        projectsJson.forEach(project => {
-            if (searchArray.includes(project.technology)) shownProjects.push(project);
-        });
-        setProjects(shownProjects);
-    }
-
     return <>
-        <Techbar handleTechBarClick={handleTechQueryButton}/>
-        {search ?
-        <div className='center'>
-            <h1 >Now Showing Projects with {search}! </h1>
-            <button className='clear-button' id='' onClick={handleTechQueryButton}>
-                Clear Search
-            </button>
-        </div> : null
-        }
+        <div>
+            <Techbar handleTechBarClick={handleTechQueryButton}/>
+            {search ?
+            <div className='center'>
+                <h1 >Now Showing Projects with {search}! </h1>
+                <button className='clear-button' id='' onClick={handleTechQueryButton}>
+                    Clear Search
+                </button>
+            </div> : null
+            }
+        </div>
 
-        {projects.map(project => 
-        
-        <Card
-            title={project.title}
-            text={project.text}
-        />)}
+        <div className="row">
+            {projects.map((project, index) => 
+                <Card
+                    key={index}
+                    title={project.name}
+                    text={project.description}
+                />)
+            }
+        </div>
 
     </>
 }
